a95789595fe1ad55f4a5868d4f81cbd304faeadf
Stephen McGuckin <48228986+guckin@users.noreply.github.com>
2023-03-10 16:06:29 -0800

    adds persistence layer and POST endpoint (#14)
    * adds persistence layer and POST endpoint. This allows us to persist content data.
    
    * adds a comment for when the code is deployed
    
    * update workflow to diff against prod
    
    * changes the workflow success comment to be more pretty
    
    * makes it so builds will get cancelled on new pushes

---
+++ src/content-query.ts
@@ -0,0 +1,7 @@
+// import Zod from 'zod';
+//
+// const queryParams = Zod.object({
+//   start: Zod.string().datetime(),
+//   end: Zod.string().datetime(),
+//   pageNextToken: Zod.string().optional()
+// })
--- src/rest-api-handler.test.ts
+++ src/rest-api-handler.test.ts
@@ -1,11 +1,67 @@
 import {RestApiHandler} from './rest-api-handler';
+import {APIGatewayProxyEvent} from 'aws-lambda/trigger/api-gateway-proxy';
 
-test('returns 200 Hello World', async () => {
-    const handler = new RestApiHandler();
 
-    const response = await handler.invoke();
-    
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toBe(JSON.stringify({content: '# Hello World'}));
+describe('returns not found if the method is not POST', () => {
+    test('PUT', async () => {
+        const event = putRequest();
+
+        const response = await handler.invoke(event);
+
+        expect(response.statusCode).toEqual(404);
+        expect(response.body).toEqual(JSON.stringify({message: 'Not Found'}));
+    });
 });
+
+describe('POST', () => {
+    test('returns 200 OK', async () => {
+        repository.save.mockResolvedValue(undefined);
+        const event = validContentPostRequest();
+
+        const response = await handler.invoke(event);
+
+        expect(response.statusCode).toEqual(201);
+        expect(response.body).toEqual(JSON.stringify({message: 'Created'}));
+    });
+
+    test('returns 422 when body is invalid', async () => {
+        const event = validContentPostRequest({text: 'Hello World'});
+
+        const response = await handler.invoke(event);
+
+        expect(response.statusCode).toEqual(422);
+    });
+});
+
+const repository = {
+    save: jest.fn(),
+    listContent: jest.fn()
+};
+
+const handler = new RestApiHandler(repository);
+
+function validContentPostRequest(body: unknown = validDummyBody): APIGatewayProxyEvent {
+    return {
+        body: JSON.stringify(body),
+        httpMethod: 'POST',
+        headers: {},
+        pathParameters: null,
+        queryStringParameters: null
+    } as APIGatewayProxyEvent;
+}
+
+function putRequest() {
+    return {
+        httpMethod: 'PUT',
+        headers: {},
+        pathParameters: null,
+        queryStringParameters: null
+    } as APIGatewayProxyEvent;
+}
+
+const validDummyBody = {
+    text: 'Hello World',
+    userId: '4a7ff0c7-fb20-44e5-bba5-f857f270616c',
+    date: '2020-01-01T00:00:00.000Z'
+};
 
